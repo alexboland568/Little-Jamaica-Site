d828c432f6a2d60192be92b02350d5c8c8857e8f
alexboland568 <91577481+alexboland568@users.noreply.github.com>
2022-05-13 22:44:59 -0500

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+
+
+
+function myMap() {
+
+    var mapProp = {
+
+        center: new google.maps.LatLng(31.112320226557003, -97.76162629557632),
+        zoom: 17,
+
+    };
+
+    var map = new google.maps.Map(document.getElementById("location-map"), mapProp);
+
+}
+
+function subscribe_newsletter() {
+
+    
+
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.listen(3000);
+
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/img", express.static(__dirname + "/public/img"));
+
+app.get("/", function(req, res) {
+
+    res.sendFile(__dirname + "/views/index.html");
+
+})
+
+app.get("/menu", function(req, res) {
+
+    res.sendFile(__dirname + "/views/menu.html");
+
+})
+
+app.get("/location", function(req, res) {
+
+    res.sendFile(__dirname + "/views/location.html");
+
+})
+
+app.get("/team", function(req, res) {
+
+    res.sendFile(__dirname + "/views/team.html");
+
+})
+
+app.get("/apply", function(req, res) {
+
+    res.sendFile(__dirname + "/views/apply.html");
+
+})
+
+app.get("/contact", function(req, res) {
+
+    res.sendFile(__dirname + "/views/contact.html");
+
+})
+console.log("Server running");
+
