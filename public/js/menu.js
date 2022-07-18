922a5ce52fe2d9405b3385e04a1d0d0d7775f871
alexboland568 <91577481+alexboland568@users.noreply.github.com>
2022-07-18 13:54:56 -0500

    Hover Menu Items

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,7 +2,8 @@ ackee_imgs = ["img/menu/ackee.jpg", "img/menu/ackee2.png"];
 
 var index = 0;
 
-var hover = 100;
+var hover = false;
+var opacity = 1;
 
 $(".container").click(function(evt) {
 
@@ -19,9 +20,42 @@ $(".container").click(function(evt) {
 
 })
 
+
+setInterval(function() {
+
+    if (hover) {
+
+        if (opacity > 0.5) {
+
+            opacity -= 0.1;
+
+        }
+
+    }
+
+    else {
+
+        if (opacity < 1) {
+
+            opacity += 0.1;
+
+        }
+
+    }
+
+}, 1000 / 60)
+
+
 $(".menu-img").hover(function() {
 
-    $(this).attr("opacity", "0.5")
+    hover = true
+    
+
+}, function() {
+
+    hover = false;
+
+    
 
 })
 
